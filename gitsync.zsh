6fab0e3b46b302fa181350fd624e5e8269d078c1
David Milum <david.milum.89@gmail.com>
2015-12-22 19:46:33 -0500

    push --all is opt-in instead of --this

--- gitsync.zsh
+++ gitsync.zsh
@@ -616,17 +616,17 @@ function gitsync() {
     shift
     case $action in
         push)
-            if [ "$1" = "--this" ]; then
-                _gitsync-push 
-            else
+            if [ "$1" = "--all" ]; then
                 ( _gitsync-push-all )
+            else
+                _gitsync-push 
             fi
             ;;
         fetch)
-            if [ "$1" = "--this" ]; then
-                ( _gitsync-fetch )
-            else
+            if [ "$1" = "--all" ]; then
                 ( _gitsync-fetch-all )
+            else
+                ( _gitsync-fetch )
             fi
             ;;
         autocommit)
